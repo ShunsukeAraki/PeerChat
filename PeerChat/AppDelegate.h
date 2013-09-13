16504bdf39ddc21cc809b416251c6283237d7569
ShunsukeAraki <araki.sh1@gmail.com>
2013-09-13 17:42:52 +0900

    Initial Commit

---
+++ PeerChat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PeerChat
+//
+//  Created by Shunsuke Araki on 2013/09/13.
+//  Copyright (c) 2013年 individual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
